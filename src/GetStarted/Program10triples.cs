db40890ac27889df0cb03fcdf426224e5a704339
agmarchuk <mag@iis.nsk.su>
2018-01-25 08:14:16 +0700

    Начал эксперимент по созданию эффективных индексов

---
+++ src/GetStarted/Program10triples.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Polar.DB;
+
+namespace GetStarted
+{
+    partial class Program
+    {
+        private static System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        public static void Main10()
+        {
+            Console.WriteLine("Start GetStarted/Main10");
+            using (Stream tab_stream = File.Open(path + "tab_stream.bin", FileMode.OpenOrCreate))
+            using (Stream spo_stream = File.Open(path + "spo_stream.bin", FileMode.OpenOrCreate))
+            {
+                Mag_Triple_Store store = new Mag_Triple_Store(tab_stream, spo_stream, null);
+
+                int ntiples = 1_000_000;
+                // Начало таблицы имен 0 - type, 1 - name, 2 - person
+                int b = 3; // Начальный индекс назначаемых идентификаторов сущностей
+
+                sw.Restart();
+                var query = Enumerable.Range(0, ntiples)
+                    .SelectMany(i => new object[]
+                    {
+                    new object[] { ntiples + b - i - 1, 0, new object[] { 1, 2 } },
+                    new object[] { ntiples + b - i - 1, 1, new object[] { 2, "pupkin" + (ntiples + b - i - 1) } }
+                    }); // по 2 триплета на запись
+                store.Load(query);
+                store.Build();
+                sw.Stop();
+                Console.WriteLine($"load of {ntiples * 2} triples ok. Duration={sw.ElapsedMilliseconds}");
+                store.Look();
+            }
+        }
+    }
+
+    // =================== библлиотека =====================
+
+    // Хранилище
+    public class Mag_Triple_Store
+    {
+        // Есть таблица имен для хранения строк IRI
+        //private Mag_Nametable nametable; // пока не используется
+
+        // Тип Object Variants
+        PType tp_ov = new PTypeUnion(
+            new NamedType("dummy", new PType(PTypeEnumeration.none)),
+            new NamedType("iri", new PType(PTypeEnumeration.integer)),
+            new NamedType("str", new PType(PTypeEnumeration.sstring)));
+        // Тип триплепта
+        PType tp_triple;
+        
+        // Основная таблица - таблица триплетов
+        private UniversalSequenceBase table;
+        // Индекс
+        private UniversalSequenceComp index_spo;
+        // Компаратор
+        Comparer<object> spo_comparer;
+        // Конструктор
+        public Mag_Triple_Store(Stream tab_stream, Stream spo_stream, Comparer<object> comp)
+        {
+            this.spo_comparer = comp;
+            tp_triple = new PTypeRecord(
+                new NamedType("subj", new PType(PTypeEnumeration.integer)),
+                new NamedType("pred", new PType(PTypeEnumeration.integer)),
+                new NamedType("obj", tp_ov));
+            table = new UniversalSequenceBase(tp_triple, tab_stream);
+            spo_comparer = Comparer<object>.Create(new Comparison<object>((object a, object b) =>
+            {
+                object[] aa = (object[])a; object[] bb = (object[])b;
+                int cmp = ((int)aa[0]).CompareTo((int)bb[0]);
+                return cmp;
+            }));
+            index_spo = new UniversalSequenceComp(new PType(PTypeEnumeration.longinteger), spo_stream, spo_comparer, table);
+        }
+
+        public void Load(IEnumerable<object> triples)
+        {
+            table.Clear();
+            foreach (object tri in triples)
+            {
+                long off = table.AppendElement(tri);
+                index_spo.AppendElement(off);
+            }
+            table.Flush();
+            index_spo.Flush();
+        }
+        public void Build()
+        {
+            int nelements = (int)table.Count();
+            object[] arr_triples = new object[nelements];
+            long[] arr_offs = new long[nelements];
+            for (int i = 0; i < nelements; i++)
+            {
+                long off = i == 0 ? 8L : table.ElementOffset();
+                arr_offs[i] = off;
+                object v = table.GetElement(off);
+                arr_triples[i] = v;
+            }
+            Array.Sort(arr_triples, arr_offs, spo_comparer);
+            index_spo.Clear();
+            for (int i = 0; i < nelements; i++) index_spo.AppendElement(arr_offs[i]);
+            index_spo.Flush();
+        }
+        public void Look()
+        {
+            foreach (long offset in index_spo.ElementValues().Take(100))
+            {
+                var v = table.GetElement(offset);
+                Console.Write($"{tp_triple.Interpret(v)} ");
+            }
+            Console.WriteLine();
+        }
+        public IEnumerable<object> GetTriplesBySubj(int subj)
+        {
+            object sample = new object[] { subj, null, null };
+            return Enumerable.Empty<object>();
+        }
+    }
+
+
+
+}
---
+++ src/Polar.DB/UniversalSequenceComp.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using Polar.DB;
+
+namespace Polar.DB
+{
+    /// <summary>
+    /// Класс предоставляет индекс, организованный на элементах типа tp_elem, опирающийся на процедуру сравнения объектных представлений элементов
+    /// </summary>
+    public class UniversalSequenceComp : UniversalSequenceBase
+    {
+        private Comparer<object> comp;
+        private UniversalSequenceBase bearing;
+        //PType tp_elem;
+        public UniversalSequenceComp(PType tp_elem, Stream media, Comparer<object> comp, UniversalSequenceBase bearing_table) : base(tp_elem, media)
+        {
+            this.comp = comp;
+            this.bearing = bearing_table;
+        }
+
+        /*
+        
+        /// <summary>
+        /// Метод выборки всех триплетов, удовлетворяющих условию компаратора
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public IEnumerable<object> BinarySearchAll(object sample)
+        {
+            return BinarySearchAll(0, this.Count(), sample);
+        }
+        public IEnumerable<object> BinarySearchAll(long start, long numb, object sample)
+        {
+            // Пропустим возможные проверки применимости
+            if (numb > 0)
+            {
+                // Начальный элемент
+                var offset = (long)this.GetElement(this.ElementOffset(start));
+                var elementFrom = bearing.GetElement(offset);
+                return BinarySearchInside(elementFrom, numb, sample);
+            }
+            return Enumerable.Empty<object>();
+        }
+
+        // Ищет все решения внутри имея ввиду, что слева за диапазоном уровень меньше нуля, справа за диапазоном больше 
+        private IEnumerable<object> BinarySearchInside(object elementFrom, long number, object sample)
+        {
+            long half = number / 2;
+            if (half > 0)
+            {
+                var size = this.elem_size;
+                PaEntry middle = new PaEntry(elementFrom.Type, elementFrom.offset + half * size, elementFrom.cell);
+                PaEntry aftermiddle = new PaEntry(elementFrom.Type, middle.offset + size, elementFrom.cell);
+                var middle_depth = elementDepth(middle);
+
+                if (middle_depth == 0)
+                {
+                    foreach (var pe in BinarySearchLeft(elementFrom, half, elementDepth)) yield return pe;
+                    yield return middle;
+                    foreach (var pe in BinarySearchRight(aftermiddle, number - half - 1, elementDepth)) yield return pe;
+                }
+                else if (middle_depth < 0)
+                {
+                    foreach (var pe in BinarySearchInside(aftermiddle, number - half - 1, elementDepth)) yield return pe;
+                }
+                else // if (middle_depth > 0)
+                {
+                    foreach (var pe in BinarySearchInside(elementFrom, half, elementDepth)) yield return pe;
+                }
+            }
+            else if (number == 1) // && half == 0) - возможно одно решение или их нет
+            {
+                if (elementDepth(elementFrom) == 0) yield return elementFrom;
+            }
+        }
+
+
+        // Ищет все решения имея ввиду, что справа решения есть 
+        private static IEnumerable<PaEntry> BinarySearchLeft(PaEntry elementFrom, long number, Func<PaEntry, int> elementDepth)
+        {
+            long half = number / 2;
+            if (half > 0)
+            {
+                var size = elementFrom.Type.HeadSize;
+                PaEntry middle = new PaEntry(elementFrom.Type, elementFrom.offset + half * size, elementFrom.cell);
+                PaEntry aftermiddle = new PaEntry(elementFrom.Type, middle.offset + size, elementFrom.cell);
+                var middle_depth = elementDepth(middle);
+
+                if (middle_depth == 0)
+                {
+                    foreach (var pe in BinarySearchLeft(elementFrom, half, elementDepth)) yield return pe;
+                    yield return middle;
+                    // Переписать все из второй половины
+                    for (long ii = 0; ii < number - half - 1; ii++)
+                    {
+                        yield return aftermiddle;
+                        aftermiddle = new PaEntry(elementFrom.Type, aftermiddle.offset + size, elementFrom.cell);
+                    }
+                }
+                else if (middle_depth < 0)
+                {
+                    foreach (var pe in BinarySearchLeft(aftermiddle, number - half - 1, elementDepth)) yield return pe;
+                }
+                else throw new Exception("Assert err: 9283");
+            }
+            else if (number == 1) // возможно одно решение или их нет
+            {
+                if (elementDepth(elementFrom) == 0) yield return elementFrom;
+            }
+        }
+        // Ищет все решения имея ввиду, что слева решения есть 
+        private static IEnumerable<PaEntry> BinarySearchRight(PaEntry elementFrom, long number, Func<PaEntry, int> elementDepth)
+        {
+            long half = number / 2;
+            if (half > 0)
+            {
+                var size = elementFrom.Type.HeadSize;
+                PaEntry middle = new PaEntry(elementFrom.Type, elementFrom.offset + half * size, elementFrom.cell);
+                PaEntry aftermiddle = new PaEntry(elementFrom.Type, middle.offset + size, elementFrom.cell);
+                var middle_depth = elementDepth(middle);
+
+                if (middle_depth == 0)
+                {
+                    // Переписать все из первой половины
+                    PaEntry ef = elementFrom;
+                    for (long ii = 0; ii < half; ii++)
+                    {
+                        yield return ef;
+                        ef = new PaEntry(elementFrom.Type, ef.offset + size, elementFrom.cell);
+                    }
+                    yield return middle;
+                    foreach (var pe in BinarySearchRight(aftermiddle, number - half - 1, elementDepth)) yield return pe;
+                }
+                else if (middle_depth > 0)
+                {
+                    foreach (var pe in BinarySearchRight(elementFrom, half, elementDepth)) yield return pe;
+                }
+            }
+            else if (number == 1) // возможно одно решение или их нет
+            {
+                if (elementDepth(elementFrom) == 0) yield return elementFrom;
+            }
+        }
+        */
+
+    }
+}
