2e1f438b4329177717125fef44dd6183978a74e7
Alexander G. Marchuk <mag@iis.nsk.su>
2020-08-28 06:48:34 +0300

    после отпуска

---
+++ src/GetStarted2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GetStarted2
+{
+    partial class Program
+    {
+        private static string dbpath = @"D:/Home/data/GetStarted/";
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Main201();
+        }
+    }
+}
